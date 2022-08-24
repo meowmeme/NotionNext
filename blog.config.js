bf98c228def3b447d82bce6dab2812baa115265c
meowmeme <104475898+meowmeme@users.noreply.github.com>
2022-08-24 17:04:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
   AUTHOR: 'shuusip', // 作者
-  BIO: '一个普通的干饭人🍚', // 作者简介
+  BIO: '饿货', // 作者简介
   LINK: 'https://chuuchip.blog.edu.kg', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID: 
